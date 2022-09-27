096b2c4a502eb981a2ec60b7dda5afd162ceac95
dev-th-kang <kang.1107.th@gmail.com>
2022-09-28 00:00:18 +0900

    update baekjoon solved

--- baekjoon/9205(맥주 마시면서 걸어가기)/9205.cpp
+++ baekjoon/9205(맥주 마시면서 걸어가기)/9205.cpp
@@ -1,26 +1,62 @@
 #include <iostream>
+#include <cstring>
+#include <queue>
+// 절대값
+#define absV(x) (((x) > 0)? (x): -(x))
 using namespace std;
-int n,m;
-int arr[10];
-bool visitPoint[10];
-void dfs(int k){
-    if(k==m){
-        for(int i=0;i<m;i++) cout << arr[i] << " ";
-        cout <<"\n";
-        return;    
-    }
 
-    for(int i=1; i<=n;i++){
-        if(!visitPoint[i]){
-            arr[k] = i;
-            visitPoint[i] = true;
-            dfs(k+1);
-            visitPoint[i] = false;
-        }
-    }
+struct POINT {
+	int x;
+	int y;
+};
+POINT store[100];
+POINT goal;
+POINT home;
+
+int N;
+bool visited[100];
+
+// 편의점 없이 갈 수 있는 최대 거리 1,000m (50 m * 20)
+void bfsSearch() {
+	queue <POINT> q;
+	q.push(home); // 시작
+
+	while (!q.empty()) {
+		int x = q.front().x;
+		int y = q.front().y;
+		q.pop();
+
+		if (absV(x - goal.x) + absV(y - goal.y) <= 1000) {
+			printf("happy\n");
+			return;
+		}
+
+		//가게!
+		for (int i = 0; i < N; i++) {
+			if (visited[i] == true)continue;
+			int d = absV(x - store[i].x) + absV(y - store[i].y);
+			if (d <= 1000) {
+				visited[i] = true;
+				q.push({ store[i].x, store[i].y });
+			}
+		}
+	}
+
+	printf("sad\n");
+	return;
 }
-int main(){
-    cin >> n >> m;
 
-
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		memset(visited, 0, sizeof(visited)); 
+		cin >> N;
+		cin >> home.x >> home.y;
+		for (int i = 0; i < N; i++) {
+			cin >> store[i].x >> store[i].y;
+		}
+		cin >> goal.x >> goal.y;
+		bfsSearch();
+	}
 }
